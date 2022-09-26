06ccf7f32d9c7b0f687e4f4a9cd8b690955326ea
casatoo <casato6666@gmail.com>
2022-09-26 17:27:35 +0900

    게시글 테스트 데이터 생성, 글 삭제 기능 구현

--- src/main/java/com/KMS/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/KMS/exam/demo/controller/UsrArticleController.java
@@ -7,33 +7,73 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.KMS.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-	int articleId = 0;
-	
-	@Data
-	@NoArgsConstructor
-	@AllArgsConstructor
-	class Article {
-		private int id;
-		private String title;
-		private String body;
+
+	private int lastArticleId;
+	private List<Article> articles;
+
+	public UsrArticleController() {
+		lastArticleId = 0;
+		articles = new ArrayList<>();
+
+		makeTestData();
+	}
+    private Article getArticle(int id){
+        for(Article article : articles){
+            if(article.getId()==id){
+                return article;
+            }
+        }
+        return null;
+    }
+    private void deleteArticle(int id) {
+    	Article article = getArticle(id);
+    	
+    	articles.remove(article);
+    }
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = lastArticleId + 1;
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+
+			writeArticle(title, body);
+		}
 	}
 	
+	private Article writeArticle(String title, String body) {
+		int id = lastArticleId + 1;
+		Article article = new Article(id, title, body);
+		return article;
+	}
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		int id = articleId+1;
-		Article article = new Article(id, title, body);
-
-		List<Article> articles = new ArrayList<>();
-		articles.add(article);
+		int id = lastArticleId + 1;
+		Article article = writeArticle(title, body);
 		
 		return article;
+	}
+
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		return articles;
+	}
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticle(id);
+		if(article == null) {
+			return id+"번 게시물은 존재하지 않습니다.";
+		}
+		deleteArticle(id);
+		return id+"번 게시물을 삭제했습니다.";
 	}
 	
 }
