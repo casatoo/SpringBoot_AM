0ca561321150bb5e871cedf25e7fde63c2e24140
casatoo <casato6666@gmail.com>
2022-09-27 18:47:36 +0900

    기능 쿼리문 작성

--- DB.sql
+++ DB.sql
@@ -17,3 +17,12 @@ INSERT INTO article(regDate,updateDate,title,`body`)VALUES
 (NOW(),NOW(),'제목3','내용3'),
 (NOW(),NOW(),'제목4','내용4');
 
+DELETE FROM article WHERE id = 5;
+
+UPDATE article SET 
+updateDate = NOW(),
+title = '제목6',
+`body` = '내용6'
+WHERE id = 1;
+
+SELECT LAST_INSERT_ID();
--- src/main/java/com/KMS/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/KMS/exam/demo/repository/ArticleRepository.java
@@ -2,22 +2,29 @@ package com.KMS.exam.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.KMS.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-
-	public Article writeArticle(String title, String body);
+	
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	public void writeArticle(String title, String body);
 
 	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(int id);
 	
+	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
-
+	
+	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(int id);
-
+	
+	@Update("UPDATE article SET updateDate = NOW(),title = #{title}, `body` = #{body} WHERE id = #{id}")
 	public void modifyArticle(int id, String title, String body);
 }
